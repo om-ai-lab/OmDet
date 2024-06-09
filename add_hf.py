957a0d78c4227636e9e1e7832450aca99ad2990f
Niels <niels.rogge1@gmail.com>
2024-06-09 19:26:46 +0200

    Add script

---
+++ add_hf.py
@@ -0,0 +1,18 @@
+from omdet.infernece.det_engine import DetEngine
+from omdet.omdet_v2_turbo.detector import OmDetV2Turbo
+
+
+if __name__ == "__main__":
+    engine = DetEngine(batch_size=1, device='cuda')
+    img_paths = ['./sample_data/000000574769.jpg']       # path of images
+    labels = ["person", "cat", "orange"]          # labels to be predicted
+    prompt = 'Detect {}.'.format(','.join(labels))        # prompt of detection task, use "Detect {}." as default
+
+    model_id = 'OmDet-Turbo_tiny_SWIN_T'
+    model, cfg = engine._load_model(model_id)
+
+    # push to hub
+    model.push_to_hub("nielsr/omde-v2-turbo-tiny-swin-tiny")
+
+    # reload
+    model = OmDetV2Turbo.from_pretrained("nielsr/omde-v2-turbo-tiny-swin-tiny")
